bd6c9853f081d0c1280a5b688a39fd523afa1d13
Bhaarat Sharma <bhaarat.sharma@aerstone.com>
2016-10-13 09:14:53 -0400

    initial

---
+++ hello.py
@@ -0,0 +1,21 @@
+"""Cloud Foundry test"""
+from flask import Flask
+import os
+from ctypes import *
+
+app = Flask(__name__)
+
+port = int(os.getenv("PORT"))
+
+@app.route('/')
+def hello_world():
+    try:
+      self.leptonica = cdll.LoadLibrary(".heroku/vendor/lib/liblept.so.4.0.2")
+      return 'Hello World! loaded liblept'
+    except (OSError):
+      logger.info("ERROR LOADING leptlib: " + leptlib)
+      return 'Hello World! could not load liblept'
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
